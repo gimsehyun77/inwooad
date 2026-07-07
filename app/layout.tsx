b4ea9d30b3c7d78f332575cd2dc1e54d61363779
gimsehyun77 <gimsehyun77@gmail.com>
2026-07-07 18:49:46 +0900

    Fix Google Ads base tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
   import { Geist, Geist_Mono } from "next/font/google";
   import "./globals.css";
   import Script from "next/script";
+  import { Analytics } from "@vercel/analytics/react";
 
   const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -79,6 +80,7 @@ gtag('config', 'AW-18299011325');
 `}
 </Script>
           {children}
+          <Analytics />
           </body>
       </html>
     );
