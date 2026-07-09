30039cfc9ab7a08ec7f4a8aaa376514ee6607309
gimsehyun77 <gimsehyun77@gmail.com>
2026-07-09 12:58:59 +0900

    Fix photozone photo and url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://inwooad.vercel.app";
+  const baseUrl = "https://inwooad.net";
 
   return [
     {
